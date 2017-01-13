bf469a9ab619af1a5c0c71211551ac9bb9dd993e
Paweł Rudnicki <pawelrudnicki79@gmail.com>
2017-01-13 18:22:04 +0100

    dodanie nowych konfiuguracj poczty

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,11 +89,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
-    :port => 465,
-    :user_name => 'noreplay@zlecenieit.herokuapp.com',
-    :password => 'noreplay078258',
-    :authentication => "plain",
+    :port => 587,
+    :user_name => 'pawelrudnicki79@gmail.com',
+    :password => 'rudnicki078258',
+    :authentication => :plain,
     :enable_starttls_auto => true
   }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.default_url_options = { :host => "zlecenieit.herokuapp.com" }
 end
