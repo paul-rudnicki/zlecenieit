7f4fde8c924aa76947c6d0022fa2a31d5ddfdfd1
Paweł Rudnicki <pawelrudnicki79@gmail.com>
2017-01-14 20:38:16 +0100

    change developer.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
-    :port => 25,
+    :port => 587,
     :domain => "zlecenieit.herokuapp.com",
     :user_name => 'noreplay@zlecenieit.herokuapp.com',
     :password => 'noreplay078258',
