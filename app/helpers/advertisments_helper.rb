610e2c7ea9107097e00c90a4b8a9155ef824ec16
Paweł Rudnicki <pawelrudnicki79@gmail.com>
2017-01-18 14:35:59 +0100

    Do końca

--- app/helpers/advertisments_helper.rb
+++ app/helpers/advertisments_helper.rb
@@ -1,2 +1,12 @@
 module AdvertismentsHelper
+	def to_end advertisment
+		days = ((advertisment.ended_date - DateTime.now).to_i)/86000
+		if days == 0
+			"#{days} dni"
+		elsif days == 1
+			"ostatni dzień"
+		elsif days > 1
+			"#{days} dni"
+		end
+	end
 end
